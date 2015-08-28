15be0a5af74f582f558e805a35698cfedfce357a
Marc Ablay <marc.ablay@gmail.com>
2015-08-29 01:31:28 +0200

    Deployed dist folder to gh-pages

--- app/scripts/controllers/navigation-controller.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by marc on 28/08/15.
- */
-angular.module('ngparseApp')
-  .controller('NavigationController', function () {
-
-});
--- app/scripts/services/parse-service.js
+++
@@ -1,6 +0,0 @@
-angular.module('ngparseApp').factory('parse', function() {
-	var appId = "";
-	var jsKey = "";
-	var parse = Parse.initialize(appId, jsKey);
-	return parse;
-});
